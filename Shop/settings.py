407a33db9d0034351badcbbb4aabe01092b68d88
priest <ami8800@gmail.com>
2022-06-09 18:08:27 +0300

    new4

--- Shop/settings.py
+++ Shop/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ezf+b#8pgve2vp(8o6y@u$f$uo!g(h7)n8h%a@ohtp9nhmuask'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['my-petshop.herokuapp.com', '127.0.0.1']
 
